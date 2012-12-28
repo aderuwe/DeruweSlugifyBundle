0c721905c18974bfde70e96a445212b63cdeac51
Alexander Deruwe <alexander.deruwe@gmail.com>
2012-12-28 17:40:10 +0100

    Initial commit

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Deruwe\SlugifyBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * Validates and merge configuration from app/config files
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('deruwe_slugify');
+
+        return $treeBuilder;
+    }
+}
---
+++ DeruweSlugifyBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Deruwe\SlugifyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DeruweSlugifyBundle extends Bundle
+{
+}
